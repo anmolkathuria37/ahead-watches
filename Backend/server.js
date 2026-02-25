dd28f51b5ce24009fa9ace9438219db4d878de3c
Anmol Kathuria <anmolkathuria37@gmail.com>
2026-02-25 13:31:14 +0530

    Changed Some Piece of code in Nodemailer

--- Backend/server.js
+++ Backend/server.js
@@ -83,11 +83,23 @@ const Waitlist = mongoose.model("Waitlist", WaitlistSchema);
 /* =======================
    Nodemailer Setup
 ======================= */
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS, // Gmail App Password
+//   },
+// });
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // IMPORTANT: false for 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS, // Gmail App Password
+  },
+  tls: {
+    rejectUnauthorized: false, // helps in cloud envs
   },
 });
 
